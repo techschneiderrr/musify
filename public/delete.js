76dda12920c6bd6e7d1760dca2f2df4b6840954e
Manav Deep Singh Lamba <56251839+techschneiderrr@users.noreply.github.com>
2021-01-17 20:22:52 +0530

    Update delete.js

--- public/delete.js
+++ public/delete.js
@@ -12,7 +12,7 @@ $(".delete").on("click", function () {
     });
     location.reload()
     //alert("Deleted successfully");
-    erfgrgnfghtgfnre
+    erfgrgnf
 });
 //click function
 $(".overlay").on('click', function(){
