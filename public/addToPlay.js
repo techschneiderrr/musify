07394687d5e6645d79657de10dc705d612f98258
Manav Deep Singh Lamba <56251839+techschneiderrr@users.noreply.github.com>
2021-01-26 02:13:22 +0530

    Update addToPlay.js

--- public/addToPlay.js
+++ public/addToPlay.js
@@ -1,6 +1,6 @@
  $("#firstSong").on('click', function (e) {
      e.preventDefault();
-     var data = {
+     var data = {hkvbkh,jbnjm
          audioSrc: $("#audioSrcOne").attr("data"),
          songName: $("#songNameOne").attr("value"),
          image: $("#songImageOne").attr("src"),
