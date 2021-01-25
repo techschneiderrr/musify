e379c8f0d90d8cc2bb51fb6988dce2fa20a32b44
Manav Deep Singh Lamba <56251839+techschneiderrr@users.noreply.github.com>
2021-01-26 02:12:54 +0530

    Update addToPlay.js

--- public/addToPlay.js
+++ public/addToPlay.js
@@ -4,7 +4,7 @@
          audioSrc: $("#audioSrcOne").attr("data"),
          songName: $("#songNameOne").attr("value"),
          image: $("#songImageOne").attr("src"),
-     };efwjbjkb.jkb.hklbkhb.$
+     };efwjbj
      $.ajax({
          url: "/playlist/add",
          method: "POST",
