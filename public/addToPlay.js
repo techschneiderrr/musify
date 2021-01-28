c48f161f0b7451f1d47e5c4e297191e5beda7f64
Manav Deep Singh Lamba <56251839+techschneiderrr@users.noreply.github.com>
2021-01-28 18:35:15 +0530

    Update addToPlay.js

--- public/addToPlay.js
+++ public/addToPlay.js
@@ -13,7 +13,7 @@
          data: JSON.stringify(data),
          success: function (data) {
              console.log(data);
-         }adsfdgfdgdfggffbddgbxcdffzfddrgf dssfcvdfzdfd
+         }adsfdgfdgdfdzdzcvggffbddgbxcdffzfddrgf dssfcvdfzdfd
      });
      window.location.replace("/playlist");
  });
